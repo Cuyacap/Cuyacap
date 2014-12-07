68a79830a850d4a6aa88aa3009fd3b43d2e8676a
Brent A. Ritterbeck <cuyacap@gmail.com>
2014-12-07 13:30:12 -0800

    Committing main program file.

---
+++ cuyacap.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+namespace Cuyacap
+{
+    int
+    main(int argc, char **argv)
+    {
+        std::cout << "**********************" << std::endl;
+        std::cout << "* Welcome to Cuyacap *" << std::endl;
+        std::cout << "**********************" << std::endl;
+
+        return 0;
+    }
+}
